95fbd00e86989f41f448b51badb829e8bad4ff46
shiyunlian <shiyunlian00@gmail.com>
2021-06-01 13:50:17 -0700

    create react app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Typography} from '@material-ui/core'
+
+const App=()=>{
+    return(
+        <div>
+            <Typography varient='h1'>hello</Typography>
+        </div>
+    )
+}
+
+export default App
