968389d8be4b037886d7d84c1e2ebea2d9b8b68f
shiyunlian <shiyunlian00@gmail.com>
2021-06-01 18:31:45 -0700

    adjust cards

--- src/App.js
+++ src/App.js
@@ -15,32 +15,39 @@ import {
 import PhotoCameraIcon from "@material-ui/icons/PhotoCamera";
 import { makeStyles } from "@material-ui/core/styles";
 
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // create a hook called useStyles, that is equal to a function called makeStyles
 // pass a callback function that returns an object containing all the styles
 const useStyles = makeStyles((theme) => ({
   container: {
     backgroundColor: theme.palette.background.paper, //material ui built-in color: white
-    padding: theme.spacing(8, 0, 6),
+    padding: 30,
   },
   icon: {
     marginRight: "20px",
   },
   button: {
-    marginTop: "40px",
+    marginTop: "20px",
   },
   card: {
+    minWidth: 285,
+    fontSize: 14,
     height: "100%",
     display: "flex",
     flexDirection: "column",
   },
   cardGrid: {
-    padding: "20px",
+    padding: 10,
   },
   cardMedia: {
     paddingTop: "56.25%", // aspect ratio of 16:9
   },
   cardContent: {
     flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: "50px 0", //50px on top and bottom, 0 on left and right
   },
 }));
 
@@ -94,34 +101,44 @@ const App = () => {
         </div>
         <Container className={classes.cardGrid} maxWidth="md">
           <Grid container spacing={4}>
-            <Grid item>
-              <Card className={classes.card}>
-                <CardMedia
-                  className={classes.cardMedia}
-                  image="https://source.unsplash.com/random"
-                  title="Image title"
-                ></CardMedia>
-                <CardContent className={classes.cardContent}>
-                  <Typography gutterBottom varient="h5">
-                    Heading
-                  </Typography>
-                  <Typography gutterBottom varient="h5">
-                    This is a media card.
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size="small" color="primary">
-                    View
-                  </Button>
-                  <Button size="small" color="primary">
-                    Edit
-                  </Button>
-                </CardActions>
-              </Card>
-            </Grid>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/random"
+                    title="Image title"
+                  ></CardMedia>
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom varient="h5">
+                      Heading
+                    </Typography>
+                    <Typography gutterBottom varient="h5">
+                      This is a media card.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      View
+                    </Button>
+                    <Button size="small" color="primary">
+                      Edit
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
           </Grid>
         </Container>
       </main>
+      <footer className={classes.footer}>
+        <Typography varient="h6" align="center" gutterbottom>
+          Footer
+        </Typography>
+        <Typography varient="subtitle1" align="center" color="textSecondary">
+          Thank you
+        </Typography>
+      </footer>
     </>
   );
 };
